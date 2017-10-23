84add9afeedf072093f4cb782cb276016d5ce964
RenonDis <foobar@us.com>
2017-10-23 15:58:06 +0200

    basictabl

--- makeSncf.sql
+++ makeSncf.sql
@@ -2,4 +2,6 @@ create database sncf;
 
 use sncf;
 
-create table train ( int numero );
+create table train ( id integer primary key, numero int );
+
+create table voiture ( id integer primary key, numero int, train_id int, foreign key (train_id) references train(id) );
