87b72c05f24beac2016e7b8107ecad22f5142e41
miguel.reuter@ecl14.ec-lyon.fr <miguel.reuter@ecl14.ec-lyon.fr>
2018-01-17 10:28:50 +0100

    billet table splitted (reservation + trajet). billet view created

--- makeSncf.sql
+++ makeSncf.sql
@@ -2,7 +2,7 @@ create database sncf;
 
 use sncf;
 
-# create tables
+##########   Create tables   ##########
 create table train ( 
 numero integer primary key);
 
@@ -11,15 +11,6 @@ numero int,
 places_dispo int, 
 numero_train int, foreign key (numero_train) references train(numero),
 primary key (numero, numero_train) );
-
-/*
-create table place ( 
-numero int, 
-cote_fenetre bool, 
-voiture_numero int, foreign key (voiture_numero) references voiture(numero), 
-train_numero int, foreign key (train_numero) references train(numero),
-primary key(numero, voiture_numero, train_numero) );
-*/
 
 create table reduction (
 reduction_type char(15) primary key, 
@@ -33,6 +24,28 @@ reduction_type char(15),
 id int primary key, 
 foreign key (reduction_type) references reduction(reduction_type) );
 
+create table trajet (
+id int unique,
+numero_train int, foreign key (numero_train) references train(numero),
+ville_depart char(15), 
+gare_depart char(15), 
+ville_arrivee char(15), 
+gare_arrivee char(15),
+date_depart date, 
+heure_depart time, 
+date_arrivee date, 
+heure_arrivee time,
+prix float);
+
+create table reservation (
+id int unique,
+id_client int, foreign key (id_client) references client_sncf(id),
+id_trajet int, foreign key (id_trajet) references trajet(id),
+numero_place int,
+numero_voiture int, foreign key (numero_voiture) references voiture(numero));
+
+# table à supprimer
+/*
 create table billet( 
 numero int primary key, 
 id_client int, foreign key (id_client) references client_sncf(id),
@@ -47,7 +60,7 @@ date_depart date,
 heure_depart time, 
 date_arrivee date, 
 heure_arrivee time,
-prix float);
+prix float);*/
  
 ##########   Création Admin   ##########
 grant all privileges on sncf.* to sncfadmin@'localhost' identified by 'mypassword';
@@ -67,6 +80,7 @@ insert into train () values (2);
 insert into train () values (3);
 insert into train () values (4);
 insert into train () values (5);
+insert into train () values (6);
 
 # voiture
 insert into voiture () values (1, 100, 1);
@@ -79,7 +93,8 @@ insert into voiture () values (1, 100, 4);
 insert into voiture () values (2, 100, 4);
 insert into voiture () values (1, 100, 5);
 insert into voiture () values (2, 100, 5);
-
+insert into voiture () values (1, 100, 6);
+insert into voiture () values (2, 100, 6);
 # place
 # insert into place () values (45, true, 5);
 
@@ -91,18 +106,47 @@ insert into client_sncf () values ('Dupont', 'Marc', true, 'J-30', 4);
 insert into client_sncf () values ('Boille', 'Danny', false, 'Decouverte', 5);
 insert into client_sncf () values ('De-La-Tour', 'Franck', true, '-50%', 6);
 
-# billet
-insert into billet () values ( 1, 2, 10, 2, 1, 'Lyon', 'Part-Dieu', 'Marseille', 'St-Charles', '2018-02-25', '15:30:00', '2018-02-25', '17:40:00', 55.0);
-insert into billet () values ( 2, 3, 21, 2, 2, 'Valence', 'Valence-Ville', 'Lyon', 'Jean-Mace', '2018-03-02', '8:00:00', '2018-03-02', '8:55:00', 19.5);
-insert into billet () values ( 3, 4, 13, 1, 3, 'Macon', 'Macon-Gare', 'Paris', 'Gare de Lyon', '2018-01-25', '12:34:00', '2018-01-25', '14:45:00', 52.0);
-insert into billet () values ( 4, 5, 42, 1, 4, 'Toulouse', 'Gare Toulouse', 'Biarritz', 'Gare du Soleil', '2018-02-13', '5:25:00', '2018-02-13', '6:30:00', 35.0);
-insert into billet () values ( 5, 6, 53, 2, 5, 'Nancy', 'Gare Nancy', 'Metz', 'Gare Metz', '2018-05-02', '13:00:00', '2018-05-02', '13:40:00', 15.0);
-insert into billet () values ( 6, 2, 22, 2, 5, 'Nancy', 'Gare Nancy', 'Metz', 'Gare Metz', '2018-05-02', '13:00:00', '2018-05-02', '13:40:00', 15.0);
+# trajet
+insert into trajet () values ( 1, 1, 'Lyon', 'Part-Dieu', 'Valence', 'Valence-Ville', '2018-02-25', '15:30:00', '2018-02-25', '16:40:00', 15.0);
+insert into trajet () values ( 2, 2, 'Lyon', 'Part-Dieu', 'Macon', 'Macon-Gare', '2018-02-12', '8:30:00', '2018-02-12', '9:45:00', 25.0);
 
+insert into trajet () values ( 3, 3, 'Valence', 'Valence-Ville', 'Lyon', 'Part-Dieu', '2018-01-25', '12:34:00', '2018-01-25', '14:45:00', 15.0);
+insert into trajet () values ( 4, 4, 'Valence', 'Valence-Ville', 'Macon', 'Macon-Gare', '2018-03-02', '8:00:00', '2018-03-02', '9:55:00', 39.5);
+
+insert into trajet () values ( 5, 5, 'Macon', 'Macon-Gare', 'Lyon', 'Part-Dieu', '2018-01-25', '12:34:00', '2018-01-25', '13:45:00', 25.0);
+insert into trajet () values ( 6, 6, 'Macon', 'Macon-Gare', 'Valence', 'Valence-Ville', '2018-01-28', '12:34:00', '2018-01-28', '14:45:00', 52.0);
+
+# reservation
+insert into reservation () values (1, 2, 1, 10, 2);
+insert into reservation () values (2, 3, 2, 21, 2);
+insert into reservation () values (3, 1, 3, 13, 1);
+insert into reservation () values (4, 4, 4, 48, 2);
+insert into reservation () values (5, 3, 5, 9, 2);
+insert into reservation () values (6, 5, 6, 16, 1);
 
 ##########   Create Views   ##########
+
+create view billet as
+select reservation.id as numero,
+reservation.id_client,
+reservation.numero_place,
+reservation.numero_voiture,
+trajet.numero_train,
+trajet.ville_depart,
+trajet.gare_depart, 
+trajet.ville_arrivee, 
+trajet.gare_arrivee,
+trajet.date_depart, 
+trajet.heure_depart, 
+trajet.date_arrivee, 
+trajet.heure_arrivee,
+trajet.prix as 'prix non réduit'
+from reservation, trajet
+where (reservation.id_trajet = trajet.id);
+
+
 # n° train | n° voiture | places dispo (reservées ou non)
-create view diponibilites-voit as
+create view diponibilites_voit as
 select train.numero as 'n° train', 
 voiture.numero as 'n° voiture', 
 voiture.places_dispo 
@@ -130,7 +174,7 @@ from billet, train
 where billet.numero_train = train.numero;
 
 # n° train | nb voitures | nb total de places dispo (reservées ou non)
-create view diponibilites-train as
+create view disponibilites_train as
 select train.numero as 'n° train',
 (select count(*) from voiture where voiture.numero_train = train.numero) as 'nombre voitures',
 (select sum(voiture.places_dispo) from voiture where voiture.numero_train = train.numero) as 'places total dispo'
@@ -147,7 +191,7 @@ where billet.numero_train = train.numero and billet.numero_voiture = voiture.num
 
 
 # train n° | voiture n° | nb places reservées
-create view voit-reserv as
+create view voit_reserv as
 select train.numero as 'n° train',
 voiture.numero as 'n° voiture',
 (select count(*) from billet where billet.numero_train = train.numero and billet.numero_voiture = voiture.numero) as 'nb places reservées'
@@ -166,17 +210,17 @@ from train;
 
 # date depart | heure depart | ville depart
 create view prochains_departs as
-select billet.date_depart, 
-billet.heure_depart, 
-billet.ville_depart
-from billet
-where ( curdate() < billet.date_depart or (curdate() = billet.date_depart and curtime() < billet.heure_depart));
+select trajet.date_depart, 
+trajet.heure_depart, 
+trajet.ville_depart
+from trajet
+where ( curdate() < trajet.date_depart or (curdate() = trajet.date_depart and curtime() < trajet.heure_depart));
 
-# n° train | gare départ | gare d'arrivée | heure départ
+# n° train | gare départ | gare d'arrivée | date départ | heure départ
 create view train_infos as
-select distinct train.numero, billet.gare_depart, billet.gare_arrivee, billet.heure_depart
-from train, billet
-where billet.numero_train = train.numero;
+select distinct train.numero, trajet.gare_depart, trajet.gare_arrivee, trajet.date_depart, trajet.heure_depart
+from train, trajet
+where trajet.numero_train = train.numero;
 
 
 
